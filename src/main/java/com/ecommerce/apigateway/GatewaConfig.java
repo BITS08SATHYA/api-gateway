57df08ba952b108554b40de324d82b8be6736fe4
Sathya <neelasathya06@gmail.com>
2025-07-22 20:43:42 -0400

    rate limiting with redis server

--- src/main/java/com/ecommerce/apigateway/GatewaConfig.java
+++ src/main/java/com/ecommerce/apigateway/GatewaConfig.java
@@ -1,12 +1,27 @@
 package com.ecommerce.apigateway;
 
+import org.springframework.cloud.gateway.filter.ratelimit.KeyResolver;
+import org.springframework.cloud.gateway.filter.ratelimit.RedisRateLimiter;
 import org.springframework.cloud.gateway.route.RouteLocator;
 import org.springframework.cloud.gateway.route.builder.RouteLocatorBuilder;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
+import reactor.core.publisher.Mono;
 
 @Configuration
 public class GatewaConfig {
+
+    @Bean
+    public RedisRateLimiter redisRateLimiter() {
+        return new RedisRateLimiter(10,20,1);
+    }
+
+    @Bean
+    public KeyResolver hostNameKeyResolver() {
+        return exchange -> Mono.just(exchange.getRequest()
+                .getRemoteAddress().getHostName());
+    }
 
     @Bean
     public RouteLocator customRouteLocator(RouteLocatorBuilder builder) {
@@ -14,9 +29,20 @@ public class GatewaConfig {
         return builder.routes()
                 .route("product-service", r -> r
                         .path("/api/products/**")
-                        .filters(f -> f.circuitBreaker(config -> config
-                                .setName("ecomBreaker")
-                                .setFallbackUri("forward:/fallback/products")))
+                        .filters(f -> f.retry(retryConfig -> retryConfig
+                                .setRetries(10)
+                                .setMethods(HttpMethod.GET))
+                                .requestRateLimiter(config -> config
+                                        .setRateLimiter(redisRateLimiter())
+                                        .setKeyResolver(hostNameKeyResolver()))
+                                .circuitBreaker(config -> config
+                                        .setName("ecomBreaker")
+                                        .setFallbackUri("forward:/fallback/products")
+                                )
+                        )
+//                        .filters(f -> f.circuitBreaker(config -> config
+//                                .setName("ecomBreaker")
+//                                .setFallbackUri("forward:/fallback/products")))
 //                        .filters(f -> f.rewritePath("/products(?<segment>/?.*)",
 //                                "/api/products${segment}"))
                         .uri("lb://PRODUCT-SERVICE"))
